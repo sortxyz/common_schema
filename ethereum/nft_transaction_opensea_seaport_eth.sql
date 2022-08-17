2e60e8cff6e1e4777b5dcb16cdad66f3510a5a44
Jason Zucchetto <zuketo@gmail.com>
2022-08-17 14:57:35 -0700

    updated seaport views for ethereum_latest.nft_transaction

--- ethereum/nft_transaction_opensea_seaport_eth.sql
+++ ethereum/nft_transaction_opensea_seaport_eth.sql
@@ -3,13 +3,28 @@ select
     l.function_address as "nft_contract_address",
     l.params [3].value as "token_id",
     l.timestamp as "timestamp",
+    num_nfts.num as nfts_in_txn,
     l.transaction_value_eth as "amount_eth",
     'ETH' as "currency",
+    l.transaction_value_eth * l.price_eth_usd as "amount_usd",
     'OpenSea' as exchange,
     '0x00000000006c3852cbef3e08e8df289169ede581' as exchange_address,
     l.transaction_hash as "transaction_hash"
 from
-    ethereum.transaction_log l
+    ethereum_latest.transaction_log l HINT(access_path=index_filter),
+    (
+        select
+            l.transaction_hash as transaction_hash,
+      		Count(*) as num,
+        from
+            ethereum_latest.transaction_log l HINT(access_path=index_filter)
+        where
+            l.function_address = '0x00000000006c3852cbef3e08e8df289169ede581'
+      	group by l.transaction_hash
+    ) as num_nfts
 where
     l.transaction_to = '0x00000000006c3852cbef3e08e8df289169ede581'
-    and l.name = 'Approval'
+    and num_nfts.transaction_hash = l.transaction_hash
+    and l.name = 'Transfer'
+    and l.transaction_value_eth > 0
+    
--- ethereum/nft_transaction_opensea_seaport_weth.sql
+++ ethereum/nft_transaction_opensea_seaport_weth.sql
@@ -3,25 +3,52 @@ select
     l.function_address as "nft_contract_address",
     l.params [3].value as "token_id",
     l.timestamp as "timestamp",
+    num_nfts.num as nfts_in_txn,
     s.amount_weth as "amount_eth",
     'WETH' as "currency",
+    s.amount_weth * l.price_eth_usd as "amount_usd",
     'OpenSea' as exchange,
     '0x00000000006c3852cbef3e08e8df289169ede581' as exchange_address,
     l.transaction_hash as "transaction_hash"
 from
-    ethereum_latest.transaction_log l,
+    ethereum_latest.transaction_log l HINT(access_path=index_filter),
     (
         select
             l.transaction_hash,
+      		LOWER(params[1].value) as funds_source,
             sum(CAST(params [3].value as float) / 1e18) as amount_weth
         from
-            ethereum_latest.transaction_log l
+            ethereum_latest.transaction_log l HINT(access_path=index_filter)
         where
             l.function_address = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
         group by
-            l.transaction_hash
-    ) as s
+            l.transaction_hash,
+      		params[1].value
+    ) as s,
+    (
+        select
+            l.transaction_hash as transaction_hash,
+      		LOWER(params[2].value) as address
+        from
+            ethereum_latest.transaction_log l HINT(access_path=index_filter)
+        where
+            l.function_address = '0x00000000006c3852cbef3e08e8df289169ede581'
+    ) as payer,
+    (
+        select
+            l.transaction_hash as transaction_hash,
+      		Count(*) as num,
+        from
+            ethereum_latest.transaction_log l HINT(access_path=index_filter)
+        where
+            l.function_address = '0x00000000006c3852cbef3e08e8df289169ede581'
+      	group by l.transaction_hash
+    ) as num_nfts
 where
     l.transaction_hash = s.transaction_hash
+    and num_nfts.transaction_hash = l.transaction_hash
+    and payer.address = s.funds_source
+    and payer.transaction_hash = l.transaction_hash
     and l.transaction_to = '0x00000000006c3852cbef3e08e8df289169ede581'
-    and l.name = 'Approval'
+    and l.name = 'Transfer'
+    and l.function_contract_name <> 'ETH'
